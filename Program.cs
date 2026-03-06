b384a7c2f6a250bcaf4fc2879585e8aabe980920
emanu <emanu@EMANUEL_RAMREZ>
2026-03-05 21:15:38 -0600

    Logica Aplicada!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿// Escriba un programa que pida números al usuario y los sume hasta que el usuario ingrese 0.
 
+int num;
+int suma = 0;
+do
+{
 
+    Console.WriteLine("Ingrese un número");
+    num = int.Parse(Console.ReadLine());
+
+    suma = num + suma;
+
+} while (num != 0);
+
+Console.WriteLine("El resultado es = " + suma);
